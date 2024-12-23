6fcb6bccc9bde766d5be54df94dd7364c5516b22
Mehmet <mehmetygmrr02@gmail.com>
2024-12-23 23:08:10 +0300

    18

---
+++ src/main/java/com/jobportal/application/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.jobportal.application.service;
+
+import com.jobportal.application.entity.Users;
+import com.jobportal.application.repository.UsersRepository;
+import com.jobportal.application.util.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = this.usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found user"));
+        return new CustomUserDetails(user);
+    }
+}
